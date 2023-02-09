fd81673c7226423ce8e0a459a91981a5b05c496b
gustavo.hfazam <gustavo.hfazam@AME0510032W10-1>
2023-02-09 20:31:39 -0300

    Adicionar arquivos de projeto.

---
+++ natacao/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace natacao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAnoNascimento = new System.Windows.Forms.TextBox();
+            this.txtAnoUltimoAniversario = new System.Windows.Forms.TextBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnIdentificarCategoria = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(106, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtNome.Location = new System.Drawing.Point(157, 70);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(390, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(20, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ano de Nascimento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(269, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ano do Ultimo Aniversario";
+            // 
+            // txtAnoNascimento
+            // 
+            this.txtAnoNascimento.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.txtAnoNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnoNascimento.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtAnoNascimento.Location = new System.Drawing.Point(157, 141);
+            this.txtAnoNascimento.Name = "txtAnoNascimento";
+            this.txtAnoNascimento.Size = new System.Drawing.Size(100, 20);
+            this.txtAnoNascimento.TabIndex = 4;
+            // 
+            // txtAnoUltimoAniversario
+            // 
+            this.txtAnoUltimoAniversario.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.txtAnoUltimoAniversario.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnoUltimoAniversario.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtAnoUltimoAniversario.Location = new System.Drawing.Point(447, 141);
+            this.txtAnoUltimoAniversario.Name = "txtAnoUltimoAniversario";
+            this.txtAnoUltimoAniversario.Size = new System.Drawing.Size(100, 20);
+            this.txtAnoUltimoAniversario.TabIndex = 5;
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.txtCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCategoria.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtCategoria.Location = new System.Drawing.Point(447, 215);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(100, 20);
+            this.txtCategoria.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Location = new System.Drawing.Point(372, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Categoria";
+            // 
+            // btnIdentificarCategoria
+            // 
+            this.btnIdentificarCategoria.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnIdentificarCategoria.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIdentificarCategoria.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnIdentificarCategoria.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnIdentificarCategoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIdentificarCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIdentificarCategoria.Location = new System.Drawing.Point(119, 202);
+            this.btnIdentificarCategoria.Name = "btnIdentificarCategoria";
+            this.btnIdentificarCategoria.Size = new System.Drawing.Size(138, 43);
+            this.btnIdentificarCategoria.TabIndex = 8;
+            this.btnIdentificarCategoria.Text = "Identificar Categoria";
+            this.btnIdentificarCategoria.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(653, 326);
+            this.Controls.Add(this.btnIdentificarCategoria);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.txtAnoUltimoAniversario);
+            this.Controls.Add(this.txtAnoNascimento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAnoNascimento;
+        private System.Windows.Forms.TextBox txtAnoUltimoAniversario;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnIdentificarCategoria;
+    }
+}
+
